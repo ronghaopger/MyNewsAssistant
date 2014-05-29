1ad3429cb02197a82e64f27763c27833a0665577
荣浩 <ronghao_1989@hotmail.com>
2014-05-29 18:14:39 +0800

    世界杯倒计时

--- NewsGrabEngine/NewsGrabWeb/Scripts/MyJS/myjquery1-0.js
+++ NewsGrabEngine/NewsGrabWeb/Scripts/MyJS/myjquery1-0.js
@@ -12,6 +12,19 @@
         MoveDiv(e, "#hupu", "#hupu01");
     });
 
+    $("#dongqiudi").attr("DownFlag", false);
+    $("#dongqiudi").mouseenter(function () {
+        $("#dongqiudi").css({ "width": "300px", "height": "500px", "fontSize": "15px" });
+    });
+    $("#dongqiudi").mouseleave(function () {
+        $("#dongqiudi").css({ "width": "170px", "height": "500px", "fontSize": "11px" });
+        $("#dongqiudi01").css({ "marginTop": "0px" });
+        $("#dongqiudi").attr("DownFlag", false);
+    });
+    $("#dongqiudi").mousemove(function (e) {
+        MoveDiv(e, "#dongqiudi", "#dongqiudi01");
+    });
+
     $("#xinlang").attr("DownFlag", false);
     $("#xinlang").mouseenter(function () {
         $("#xinlang").css({ "width": "300px", "height": "500px", "fontSize": "15px" });
@@ -51,33 +64,59 @@
         MoveDiv(e, "#fenghuangwang", "#fenghuangwang01");
     });
     
+    $("#countTime").mouseenter(function () {
+        $("#countTime").css({ "display": "none" });
+        $("#ggormm").css({ "display": "inline" });
+    });
+    $("#gg").mouseleave(function () {
+        $("#gg").css({ "display": "none" });
+        $("#countTime").css({ "display": "inline" });
+    });
+    $("#mm").mouseleave(function () {
+        $("#mm").css({ "display": "none" });
+        $("#countTime").css({ "display": "inline" });
+    });
+    $("#ggormm").mouseleave(function () {
+        $("#countTime").css({ "display": "inline" });
+        $("#ggormm").css({ "display": "none" });
+    });
+});
 
-    //向下移动div
-    MoveDiv = function (mouse, div, div01) {
-        if ($(div)[0].clientHeight < $(div)[0].scrollHeight) {
-            if (mouse.pageY - $(div01)[0].offsetTop < 100) {
-                $(div).attr("DownFlag", false);
-                $(div01).css({ "marginTop": "0px" });
-            }
-            else if (mouse.pageY - $(div01)[0].offsetTop > 350) {
-                if ($(div).attr("DownFlag") == "true")
-                    return;
-                $(div).attr("DownFlag", true);
-                var i = 0;
-                var int = setInterval(function () {
-                    $(div01).css({ "marginTop": -i++ + "px" });
-                    if ($(div).attr("DownFlag") == "false") {
-                        clearInterval(int);
-                    }
-                    if (i == $(div)[0].scrollHeight - $(div)[0].clientHeight - 2) {
-                        clearInterval(int);
-                        $(div).attr("DownFlag", false);
-                    }
-                }, 100);
-            }
-            else {
-                $(div).attr("DownFlag", false);
-            }
+
+//向下移动div
+MoveDiv = function (mouse, div, div01) {
+    if ($(div)[0].clientHeight < $(div)[0].scrollHeight) {
+        if (mouse.pageY - $(div01)[0].offsetTop < 100) {
+            $(div).attr("DownFlag", false);
+            $(div01).css({ "marginTop": "0px" });
+        }
+        else if (mouse.pageY - $(div01)[0].offsetTop > 350) {
+            if ($(div).attr("DownFlag") == "true")
+                return;
+            $(div).attr("DownFlag", true);
+            var i = 0;
+            var int = setInterval(function () {
+                $(div01).css({ "marginTop": -i++ + "px" });
+                if ($(div).attr("DownFlag") == "false") {
+                    clearInterval(int);
+                }
+                if (i == $(div)[0].scrollHeight - $(div)[0].clientHeight - 2) {
+                    clearInterval(int);
+                    $(div).attr("DownFlag", false);
+                }
+            }, 100);
+        }
+        else {
+            $(div).attr("DownFlag", false);
         }
     }
-});
+}
+
+ggOnclick = function () {
+    $("#ggormm").css({ "display": "none" });
+    $("#gg").css({ "display": "inline" });
+};
+mmOnclick = function () {
+    $("#ggormm").css({ "display": "none" });
+    $("#mm").css({ "display": "inline" });
+}
